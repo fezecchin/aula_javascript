e2efc15a444c1b1bdb78ae106bb0d09be2c22e9c
fezecchin <zecchinfernanda@yahoo.com.br>
2021-02-05 00:42:56 -0300

    exercicios

---
+++ js/main.js
@@ -0,0 +1,57 @@
+var d = new Date();
+alert(d.getMonth()+1)
+
+// var count = 0;
+// for(count= 0; count <= 5; count++){
+//     alert(count)
+// }
+
+// var count = 0;
+// while(count <= 5){
+//     console.log(count);
+//     alert(count)
+//     count++
+    
+// }
+
+// var idade = prompt("Qual sua idade?")
+
+// if(idade >= 18){
+//     alert("é maior de idade");
+// }else{
+//     alert("menor de idade")
+// };
+
+// var frutas = 
+// [{
+//     nome:"maça",
+//     cor:"vermelho"
+// },
+//   { nome:"uva",
+//     cor: "roxo"
+//   }
+// ]
+// alert(frutas[1].nome)
+
+// var fruta = {nome: "maça", cor:"vermelha"}
+// console.log(fruta.nome)
+// alert(fruta.cor)
+
+//var lista = ["maça", "pêra", "laranja"];
+//lista.push("uva")
+//lista.pop();
+//console.log(lista.reverse())
+//console.log(lista.toString()[2]) //perde referêcia de array
+//console.log(lista.length)
+//console.log(lista.join(" - "))
+//console.log(lista[2]);
+//alert(lista[2])
+
+
+
+//var nome = "Fernanda";
+//var idade = 35;
+//var idade2 = 17;
+//alert("Bem vindo " + nome + " você tem " + idade);
+//alert(idade + idade2)
+//console.log(nome)
